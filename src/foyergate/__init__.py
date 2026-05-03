8237945ebf64589e62e231cf201e68c985024c1a
denimoll <61597946+denimoll@users.noreply.github.com>
2026-05-03 19:38:54 +0300

    Update __init__.py

--- src/foyergate/__init__.py
+++ src/foyergate/__init__.py
@@ -3,7 +3,8 @@
 Vet open-source packages before they enter your local repository.
 """
 
-from importlib.metadata import PackageNotFoundError, version as _pkg_version
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _pkg_version
 
 try:
     __version__ = _pkg_version("foyergate")
